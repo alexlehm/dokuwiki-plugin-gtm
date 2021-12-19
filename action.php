31d01fc3c4f3ceafe88f7d1eaa38d066d1969d87
Nickeau <gerardnico@users.noreply.github.com>
2021-12-19 14:07:09 +0100

    Basic test added (#3)
    * Added basic test
    global INFO was never used
    
    * The date was not consistent with dokuwiki
    https://www.dokuwiki.org/plugin:googletagmanager

--- action.php
+++ action.php
@@ -1,11 +1,13 @@
-<?php 
+<?php
 if(!defined('DOKU_INC')) die();
 if(!defined('DOKU_PLUGIN')) define('DOKU_PLUGIN',DOKU_INC.'lib/plugins/');
 require_once(DOKU_PLUGIN.'action.php');
 
 class action_plugin_googletagmanager extends DokuWiki_Action_Plugin {
 
-	/**
+    const GTMID = 'GTMID';
+
+    /**
 	 * return some info
 	 */
 	function getInfo(){
@@ -27,10 +29,10 @@ class action_plugin_googletagmanager extends DokuWiki_Action_Plugin {
 	}
 
 	function _addHeaders (&$event, $param) {
-		global $INFO;
-		if(!$this->getConf('GTMID')) return;
+
+		if(!$this->getConf(self::GTMID)) return;
 		$event->data['noscript'][] = array (
-		  '_data' => '<iframe src="//www.googletagmanager.com/ns.html?id='.$this->getConf('GTMID').'" height="0" width="0" style="display:none;visibility:hidden"></iframe>',
+		  '_data' => '<iframe src="//www.googletagmanager.com/ns.html?id='.$this->getConf(self::GTMID).'" height="0" width="0" style="display:none;visibility:hidden"></iframe>',
 		);
 		$event->data['script'][] = array (
 		  'type' => 'text/javascript',
@@ -38,7 +40,7 @@ class action_plugin_googletagmanager extends DokuWiki_Action_Plugin {
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','".$this->getConf('GTMID')."');",
+})(window,document,'script','dataLayer','".$this->getConf(self::GTMID)."');",
 		);
 	}
 }
