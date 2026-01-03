bcb1a129a382c5dce447a723df54f0f9557e49f1
Alexander Lehmann <alexlehm@gmail.com>
2026-01-03 12:26:49 +0100

    apply phpcs corrections

--- action.php
+++ action.php
@@ -36,28 +36,31 @@ class action_plugin_googletagmanager extends DokuWiki_Action_Plugin
 
         if ($is_AW_tag) {
             $event->data['script'][] = array (
-            'src' => "https://www.googletagmanager.com/gtag/js?id=" . $GTMID,
-                );
-                $event->data['script'][] = array (
+                'src' => "https://www.googletagmanager.com/gtag/js?id=" . $GTMID,
+            );
+            $event->data['script'][] = array (
                 'type' => 'text/javascript',
-                '_data' => "window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', '" .
-            $GTMID .
-            "');",
-                );
+                '_data' => "window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);}" .
+                    " gtag('js', new Date()); gtag('config', '" .
+                    $GTMID .
+                    "');",
+            );
         } else {
             $event->data['noscript'][] = array (
-            '_data' => '<iframe src="https://www.googletagmanager.com/ns.html?id=' . $GTMID . '" height="0" width="0" style="display:none;visibility:hidden"></iframe>',
-                );
-                $event->data['script'][] = array (
+                '_data' => '<iframe src="https://www.googletagmanager.com/ns.html?id=' .
+                    $GTMID .
+                    '" height="0" width="0" style="display:none;visibility:hidden"></iframe>',
+            );
+            $event->data['script'][] = array (
                 'type' => 'text/javascript',
                 '_data' => "(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','" .
-            $GTMID .
-            "');",
-                );
+                    $GTMID .
+                    "');",
+            );
         }
     }
 }
