8b69576f2c23587b30bc03355c10aeff2f67dc55
Alexander Lehmann <alexlehm@gmail.com>
2026-01-03 11:57:11 +0100

    fix unittest

--- _test/action_plugin_googletagmanagerTest.php
+++ _test/action_plugin_googletagmanagerTest.php
@@ -1,12 +1,15 @@
 <?php
 
+/**
+ * @group plugin_googletagmanager
+ * @group plugins
+ */
 
-class action_plugin_googletagmanagerTest extends DokuWikiTest
+class action_plugin_googletagmanagerTest extends \DokuWikiTest
 {
-
     const gtmPluginName = 'googletagmanager';
 
-    public function setUp()
+    public function setUp(): void
     {
         $this->pluginsEnabled[] = self::gtmPluginName;
         parent::setUp();
@@ -14,7 +17,6 @@ class action_plugin_googletagmanagerTest extends DokuWikiTest
 
     public function test_google_tag_manager()
     {
-
         global $conf;
         $gtmValue = "GTM-12345";
         $conf['plugin'][self::gtmPluginName][action_plugin_googletagmanager::GTMID] = $gtmValue;
@@ -32,29 +34,51 @@ class action_plugin_googletagmanagerTest extends DokuWikiTest
         $tagsSearched = ["script", "noscript"];
 
         foreach ($tagsSearched as $tagSearched) {
+            $domElements = $response->queryHTML($tagSearched);
 
-            $domElements = $response->queryHTML($tagSearched)->get();
-
-            $patternFound = 0;
+            $patternFound = false;
             foreach ($domElements as $domElement) {
-                /**
-                 * @var DOMElement $domElement
-                 */
-                if ($tagSearched=="script") {
-                    $value = $domElement->textContent;
-                } else {
-                    // iframe src
-                    $value = $domElement->firstChild->getAttribute("src");
+                $patternFound = false;
+                foreach ($domElements as $domElement) {
+                    if ($tagSearched=="script") {
+                        $value = $domElement->textContent;
+                    } else {
+                        // iframe src as subelement of noscript
+                        $value = $domElement->firstChild->getAttribute("src");
+                    }
+                    $patternFound = preg_match("!https://www.googletagmanager.com/.*id=.*$gtmValue!s", $value) === 1;
+                    if ($patternFound) {
+                        break;
+                    }
                 }
-                $patternFound = preg_match("/$gtmValue/i", $value);
-                if ($patternFound === 1) {
-                    break;
-                }
+                $this->assertTrue($patternFound, "The GTM scripts have not been found for the tag $tagSearched");
             }
-            $this->assertEquals(1, $patternFound, "The GTM scripts have been found for the tag $tagSearched");
         }
-
-
     }
 
+    public function test_google_tag_manager_awcode()
+    {
+        global $conf;
+        $gtmValue = "AW-12345";
+        $conf['plugin'][self::gtmPluginName][action_plugin_googletagmanager::GTMID] = $gtmValue;
+
+        $pageId = 'start';
+        saveWikiText($pageId, "Content", 'Script Test base');
+        idx_addPage($pageId);
+
+        $request = new TestRequest();
+        $response = $request->get(array('id' => $pageId, '/doku.php'));
+
+        $domElements = $response->queryHTML("script");
+
+        $patternFound = false;
+        foreach ($domElements as $domElement) {
+            $value = $domElement->textContent;
+            $patternFound = preg_match("/$gtmValue/", $value) === 1;
+            if ($patternFound) {
+                break;
+            }
+        }
+        $this->assertTrue($patternFound, "The GTM scripts have not been found");
+    }
 }
